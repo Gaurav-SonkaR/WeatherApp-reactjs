711e680fc0b6a533c5c5dc1b1cc30701e1884a57
Gaurav Sonkar <142399666+Gaurav-SonkaR@users.noreply.github.com>
2024-08-24 16:00:18 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "ee02ff6a91b181a87441397f750fdb7b",
   base: "https://api.openweathermap.org/data/2.5/",
 };
